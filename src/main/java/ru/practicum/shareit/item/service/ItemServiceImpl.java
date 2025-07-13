50abf3f92196028e4ba4c6326e42679185d52e70
Evstafev <tehnol25@yandex.ru>
2025-07-13 20:13:05 +0700

    "ТЗ 15" v.2 исправлены замечания ревьювера. 6

--- src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -189,7 +189,7 @@ public class ItemServiceImpl implements ItemService {
 
     private void setBookingDataPastAndFuture(Collection<ItemDto> itemsDto) {
 
-         Instant timeNow = LocalDateTime.now().toInstant(ZoneOffset.UTC).plusSeconds(0);  // не знаю как решить проблему. при тестирование разница в пару секунд промежутка бронирования и запроса
+         Instant timeNow = LocalDateTime.now().toInstant(ZoneOffset.UTC).minusSeconds(2);  // не знаю как решить проблему. при тестирование разница в пару секунд промежутка бронирования и запроса
 
         Collection<Long> findItems = itemsDto.stream()
                 .map(ItemDto::getId).toList();  //список вещей вернушвиеся в запросе
